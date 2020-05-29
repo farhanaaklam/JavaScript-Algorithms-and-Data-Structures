c73f02d5be22ffaf1d61d50db295a6799b2fcf67
farhana <farhanamitu70@gmail.com>
2020-05-29 15:08:33 -0400

    Check if Boolean primitive

---
+++ Basic Algorithm Scripting Js/booWho.js
@@ -0,0 +1,23 @@
+/* Basic Algorithm Scripting: Boo who
+
+Check if a value is classified as a boolean primitive. Return true or false.
+Boolean primitives are true and false. */
+
+function booWho(bool) {
+    if(bool === true || bool === false){
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+  
+  booWho(null);
+
+  /*
+  Solution-2
+  function booWho(bool) {
+    return typeof bool === "boolean";
+  } 
+  */
+  
