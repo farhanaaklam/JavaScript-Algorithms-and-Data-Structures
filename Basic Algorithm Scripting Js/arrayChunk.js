6c588ab8c1da1af03cab6070fef1f29853527bcb
farhana <farhanamitu70@gmail.com>
2020-06-10 00:26:29 -0400

    Chunk array in groups

---
+++ Basic Algorithm Scripting Js/arrayChunk.js
@@ -0,0 +1,16 @@
+/* Basic Algorithm Scripting: Chunky Monkey
+
+Write a function that splits an array (first argument) into groups the length of size (second argument)
+and returns them as a two-dimensional array. */
+
+function chunkArrayInGroups(arr, size) {
+    let newArr= [];
+    for (let i=0; i<arr.length; i+=size){
+      newArr.push(arr.slice(i,i+size));
+     // console.log(newArr);
+    }
+  return newArr;
+  }
+  
+  chunkArrayInGroups(["a", "b", "c", "d"], 2);
+  chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4);
