9cae78443294f7550dc845501124d3a96631fcb7
farhana <farhanamitu70@gmail.com>
2020-08-19 14:53:35 -0400

    Return a Sorted Array Without Changing

---
+++ Functional JavaScript/nonMutatingSort.js
@@ -0,0 +1,13 @@
+/* Use the sort method in the nonMutatingSort function to sort the elements of an array in ascending order.
+The function should return a new array, and not mutate the globalArray variable. */
+
+var globalArray = [5, 6, 3, 2, 9];
+
+function nonMutatingSort(arr) {
+  // Only change code below this line
+return [].concat(arr).sort(function(a,b){
+  return a-b;
+});
+  // Only change code above this line
+}
+nonMutatingSort(globalArray);
