1414b8ce3b09cef5a07ff28e9c3fa373b087fb2f
farhana <farhanamitu70@gmail.com>
2020-08-20 21:38:36 -0400

    Convert Strings to URL Slugs

---
+++ Functional JavaScript/convertURLslug.js
@@ -0,0 +1,13 @@
+/* Many content management sites (CMS) have the titles of a post added to part of the URL for simple
+bookmarking purposes. For example, if you write a Medium post titled "Stop Using Reduce", it's likely
+the URL would have some form of the title string in it (".../stop-using-reduce").  */
+
+// Only change code below this line
+function urlSlug(title) {
+
+    return title.toLowerCase().trim().split(/\s+/).join("-");
+    
+    }
+    // Only change code above this line
+    
+    //urlSlug(" Winter Is   Coming");
