9a04000c5e1d42ade1ed4fc7168f25a22212af81
farhana <farhanamitu70@gmail.com>
2021-08-03 16:08:35 +0600

    Another solution added

--- counting-card.js
+++ counting-card.js
@@ -54,7 +54,7 @@ cc(6);
 
 
 /* 
---------------------------------Another Solution--------------------------------
+--------------------------------Another Solution-1 --------------------------------
 function cc(card) {
     switch (card) {
       case 2:
@@ -79,6 +79,45 @@ function cc(card) {
     }
     
   } 
-  --------------------------------Another Solution--------------------------------
+  --------------------------------Another Solution-1 --------------------------------
+
+  --------------------------------Another Solution-2 --------------------------------
+  var count = 0;
+
+function cc(card) {
+  // Only change code below this line
+switch(card){
+  case 2:
+  case 3:
+  case 4:
+  case 5:
+  case 6: 
+  count = count + 1;
+  break;
+  case 7:
+  case 8:
+  case 9:
+  count = count;
+  break;
+  case 10:
+  case 'J':
+  case 'Q':
+  case 'K':
+  case 'A':
+  count = count - 1;
+  break;
+}
+
+if(count > 0){
+ return count + " Bet";
+}
+return count + " Hold";
+  // Only change code above this line
+}
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
+  --------------------------------Another Solution-2 --------------------------------
+
+
   */
 
